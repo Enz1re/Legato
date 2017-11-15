1fc71be495b50c013ced17e3752ae41761555593
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-11-15 20:56:37 +0200

    Fixed wrong event handling

--- Legato.Client/legato/Components/src/loadOnChange/LegatoLoadOnChangeDirective.ts
+++ Legato.Client/legato/Components/src/loadOnChange/LegatoLoadOnChangeDirective.ts
@@ -9,6 +9,12 @@ export class LegatoLoadOnChangeDirective implements ng.IDirective {
     restrict = "A";
     controller = "MainController";
     controllerAs = "mainCtrl";
+    private inputTextFieldTypes = [
+        "text", "password", "email", "number", "url", "search"
+    ];
+    private inputBoxTypes = [
+        "checkbox", "radio"
+    ];
 
     constructor(private onChangeService: IPendingTaskService) {
 
@@ -19,20 +25,24 @@ export class LegatoLoadOnChangeDirective implements ng.IDirective {
         const isForm = element.prop("tagName") === "FORM";
         const formName = isForm ? element.prop("name") : null;
 
-        element.find("input").on("input", e => {
-            this.onChangeService.cancelPendingTask();
-
-            if (!isForm || (isForm && scope[formName].$valid)) {
-                this.onChangeService.setPendingTask(mainCtrl.broadcastRequestEvent.bind(mainCtrl));
-            }
+        this.getInputTextFields(element).forEach(obj => {
+            obj.on("input", e => {
+                this.onChangeService.cancelPendingTask();
+                
+                if (!isForm || (isForm && scope[formName].$valid)) {
+                    this.onChangeService.setPendingTask(mainCtrl.broadcastRequestEvent.bind(mainCtrl));
+                }
+            });
         });
 
-        element.find("input").on("change", e => {
-            this.onChangeService.cancelPendingTask();
-
-            if (!isForm || (isForm && scope[formName].$valid)) {
-                this.onChangeService.setPendingTask(mainCtrl.broadcastRequestEvent.bind(mainCtrl));
-            }
+        this.getInputBoxFields(element).forEach(obj => {
+            obj.on("change", e => {
+                this.onChangeService.cancelPendingTask();
+                
+                if (!isForm || (isForm && scope[formName].$valid)) {
+                    this.onChangeService.setPendingTask(mainCtrl.broadcastRequestEvent.bind(mainCtrl));
+                }
+            });
         });
 
         element.find("select").on("change", e => {
@@ -45,5 +55,41 @@ export class LegatoLoadOnChangeDirective implements ng.IDirective {
         const directive: ng.IDirectiveFactory = (onChangeService: IPendingTaskService) => new LegatoLoadOnChangeDirective(onChangeService);
         directive.$inject = ["PendingTaskService"];
         return directive;
+    }
+
+    private getInputTextFields(container: JQLite) {
+        const allInputs = container.find("input");
+        let filtered = [];
+        
+        angular.forEach(allInputs, (value, key) => {
+            const jqueryObj = angular.element(value);
+            if (this.isTextField(jqueryObj)) {
+                filtered.push(jqueryObj);
+            }
+        });
+        
+        return filtered;
+    }
+
+    private getInputBoxFields(container: JQLite) {
+        const allInputs = container.find("input");
+        let filtered = [];
+        
+        angular.forEach(allInputs, (value, key) => {
+            const jqueryObj = angular.element(value);
+            if (this.isBoxField(jqueryObj)) {
+                filtered.push(jqueryObj);
+            }
+        });
+        
+        return filtered;
+    }
+
+    private isTextField(input: JQLite) {
+        return this.inputTextFieldTypes.indexOf(input.attr("type")) !== -1;
+    }
+
+    private isBoxField(input: JQLite) {
+        return this.inputBoxTypes.indexOf(input.attr("type")) !== -1;
     }
 }
