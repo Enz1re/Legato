76c0997405b7c3d61d0c29dda732703e74f7a03a
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-11-17 18:10:05 +0200

    Changed vendors parameter type from 'string' to 'Vendor[]'

--- Legato.Client/legato/Interfaces/src/IGuitarService.ts
+++ Legato.Client/legato/Interfaces/src/IGuitarService.ts
@@ -1,16 +1,17 @@
 ﻿import {
+    Price,
     Guitar,
     Paging,
-    Price
+    Vendor
 } from "../../Models/models";
 
 import { IService } from "../../Interfaces/interfaces";
 
 
 export interface IGuitarService<TGuitar extends Guitar> extends IService {
-    getGuitars(price: Price, vendors: string[], paging: Paging): ng.IPromise<TGuitar[]>;
+    getGuitars(price: Price, vendors: Vendor[], paging: Paging): ng.IPromise<TGuitar[]>;
 
-    getSortedGuitars(price: Price, vendors: string[], paging: Paging, sortHeader: string, sortDirection: string): ng.IPromise<TGuitar[]>;
+    getSortedGuitars(price: Price, vendors: Vendor[], paging: Paging, sortHeader: string, sortDirection: string): ng.IPromise<TGuitar[]>;
 
-    getAmount(price: Price, vendors: string[]): ng.IPromise<number>;
+    getAmount(price: Price, vendors: Vendor[]): ng.IPromise<number>;
 }
