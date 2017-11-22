aedc1bf031351db8f5f0fc7877917973c1bdd33d
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-11-22 18:42:47 +0200

    Formatted file

--- Legato.Client/legato/Models/models.ts
+++ Legato.Client/legato/Models/models.ts
@@ -1,32 +1,32 @@
-﻿import Guitar from "./src/Guitar";
-import BassGuitar from "./src/BassGuitar";
-import ClassicalGuitar from "./src/ClassicalGuitar";
+﻿import ClassicalGuitar from "./src/ClassicalGuitar";
 import ElectricGuitar from "./src/ElectricGuitar";
 import WesternGuitar from "./src/WesternGuitar";
-import Price from "./src/Price";
-import Sorting from "./src/Sorting";
-import Vendor from "./src/Vendor";
-import VendorList from "./src/VendorList";
+import BassGuitar from "./src/BassGuitar";
 import GuitarList from "./src/GuitarList";
+import VendorList from "./src/VendorList";
+import UrlParams from "./src/UrlParams";
+import Sorting from "./src/Sorting";
+import Guitar from "./src/Guitar";
+import Vendor from "./src/Vendor";
 import Amount from "./src/Amount";
 import Paging from "./src/Paging";
 import Filter from "./src/Filter";
-import UrlParams from "./src/UrlParams";
+import Price from "./src/Price";
 
 
 export {
-    Guitar,
-    BassGuitar,
     ClassicalGuitar,
     ElectricGuitar,
     WesternGuitar,
-    Price,
-    UrlParams,
-    Filter,
-    Sorting,
-    Vendor,
-    VendorList,
+    BassGuitar,
     GuitarList,
+    VendorList,
+    UrlParams,
+    Sorting,
     Amount,
-    Paging
+    Guitar,
+    Filter,
+    Vendor,
+    Paging,
+    Price
 };
