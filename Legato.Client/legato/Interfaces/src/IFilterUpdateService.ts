917caba711d681414fd53cd62b589c73dcf56fc3
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-11-28 19:48:02 +0200

    Moved some functionality from ControllerBase

--- Legato.Client/legato/Interfaces/src/IFilterUpdateService.ts
+++ Legato.Client/legato/Interfaces/src/IFilterUpdateService.ts
@@ -6,11 +6,21 @@
 
 
 export interface IFilterUpdateService {
-    filter: { price: Price, vendors: Vendor[], sorting: Sorting };
+    filter: { price: Price, vendors: Vendor[], sorting: Sorting, search: string };
 
     replacePriceQueryParams(stateName: string);
 
     replaceVendorQueryParams(stateName: string);
 
+    replaceSearchQueryParams(stateName: string);
+
     replaceSortingQueryParams(stateName: string);
+
+    needUsePriceFilter(thisVal, prevVal);
+
+    needUseVendorFilter(thisVal, prevVal);
+
+    needSearch(thisVal, prevVal);
+
+    needUseSorting(thisVal, prevVal);
 }
