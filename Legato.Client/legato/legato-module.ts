bea9114db9907002f1da65d558bd81cd18560d80
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-11-29 20:18:26 +0200

    Added restriction configuration

--- Legato.Client/legato/legato-module.ts
+++ Legato.Client/legato/legato-module.ts
@@ -2,6 +2,7 @@
 import uiRouter from "angular-ui-router";
 import ngAnimate from "angular-animate";
 import uiBootstrap from "angular-ui-bootstrap";
+import ngCookies from "angular-cookies";
 
 import services from "./Services/services-module";
 import components from "./Components/components-module";
@@ -9,8 +10,22 @@ import filters from "./Filters/filters-module";
 
 import Router from "./Routes/Router";
 
-angular.module("legato", [uiRouter, ngAnimate, uiBootstrap, services, components, filters])
-       .config(Router);
+angular.module("legato", [ngCookies, uiRouter, ngAnimate, uiBootstrap, services, components, filters])
+    .config(Router)
+    .run(["$rootScope", "$state", "$cookies", "$http", ($rootScope, $state: ng.ui.IStateService, $cookies: ng.cookies.ICookiesService, $http: ng.IHttpService) => {
+        $rootScope.globals = $cookies.getObject("globals") || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common["Authorization"] = `Basic ${$rootScope.globals.currentUser.authData}`;
+        }
+
+        $rootScope.$on("$stateChangeStart", (event: ng.IAngularEvent, toState: ng.ui.IState, toParams, fromState: ng.ui.IState, fromParams) => {
+            const isRestrictedPage = toState.name.toLowerCase() === "admin";
+            const isLoggedIn = $rootScope.globals.currentUser;
+            if (isRestrictedPage && !isLoggedIn) {
+                $state.go("login");
+            }
+        });
+    }]);
 
 angular.element(document).ready(() => {
     angular.bootstrap(document.documentElement, ["legato"]);
