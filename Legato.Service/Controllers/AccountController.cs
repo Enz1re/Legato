8dddfed06eacd012e24c5b430c086c7171b859ad
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-04 17:57:16 +0200

    Added AccountController

---
+++ Legato.Service/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Http;
+
+
+namespace Legato.Service.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Account")]
+    public class AccountController : ApiController
+    {
+        private const int TokenExpiryMinutes = 40;
+
+        public AccountController()
+        {
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        [AllowAnonymous]
+        public IHttpActionResult Login(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return BadRequest(Constants.Strings.UsernameAndPasswordAreRequired);
+            }
+            if (username.ToLower() != "admin" && password.ToLower() != "admin")
+            {
+                return BadRequest(Constants.Strings.UsernameIsIncorrect(username));
+            }
+
+            return Ok(JwtManager.GenerateToken(username, TokenExpiryMinutes));
+        }
+    }
+}
