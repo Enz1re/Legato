0f31e8e74056517a9340b55033a15cb9f383ebf0
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-05 20:18:06 +0200

    Added fallback from OPTIONS requests

--- Legato.Service/Global.asax.cs
+++ Legato.Service/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
 using System.Web.Http;
 using Legato.ServiceDAL;
 using System.Web.Routing;
@@ -12,11 +14,23 @@ namespace Legato.Service
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             ServiceMappings.CreateMappings();
+            GlobalConfiguration.Configuration.EnsureInitialized();
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept, Origin, Authorization");
+                HttpContext.Current.Response.End();
+            }
         }
     }
 }
