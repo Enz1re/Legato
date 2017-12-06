90aca692cac3915b48ea0c6bde174ece0ef847fa
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-06 20:15:30 +0200

    Added id property to Guitar model

--- Legato.Client/legato/Models/src/Guitar.ts
+++ Legato.Client/legato/Models/src/Guitar.ts
@@ -2,9 +2,14 @@
 
 
 export default class Guitar {
+    private _id: number;
     vendor: Vendor;
     model: string;
     mensura: number;
     price: number;
     imgPath: string;
+
+    get id() {
+        return this._id;
+    }
 }
