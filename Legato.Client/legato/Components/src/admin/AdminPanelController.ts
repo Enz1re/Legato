3c485c27ffbd9a63c22671a8a1802d72972f53af
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-06 20:11:17 +0200

    Added admin panel

---
+++ Legato.Client/legato/Components/src/admin/AdminPanelController.ts
@@ -0,0 +1,24 @@
+﻿import { Guitar } from "../../../Models/models";
+
+import { IManageService, IModalService } from "../../../Interfaces/interfaces";
+
+
+export class AdminPanelController {
+    static $inject = ["ManageService", "ModalService"];
+
+    constructor(private manageService: IManageService, private modalService: IModalService) {
+
+    }
+
+    addGuitar() {
+        this.modalService.openGuitarAddOrEditModal().result.then((guitar: Guitar) => {
+            console.log(guitar);
+        });
+    }
+
+    changeDisplayAmount() {
+        this.modalService.openDisplayAmountModal().result.then((amount: number) => {
+            console.log(amount);
+        })
+    }
+}
---
+++ Legato.Client/legato/Components/src/admin/LegatoAdminPanelDirective.ts
@@ -0,0 +1,11 @@
+﻿export class LegatoAdminPanelDirective implements ng.IDirective {
+    restrict = "E";
+    templateUrl = "legato/Components/src/admin/admin.html"
+    controller: "AdminPanelController";
+    controllerAs: "adminCtrl";
+
+    static create(): ng.IDirectiveFactory {
+        const directive: ng.IDirectiveFactory = () => new LegatoAdminPanelDirective();
+        return directive;
+    }
+}
