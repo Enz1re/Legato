73205884138a58679b428196d7c408f761945422
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-11 19:12:26 +0200

    Removed unused imports

--- Legato.Client/legato/Filters/filters-module.ts
+++ Legato.Client/legato/Filters/filters-module.ts
@@ -1,9 +1,6 @@
 ﻿import angular from "angular";
 
 import BooleanFilter from "./src/BooleanFilter";
-import PriceFilter from "./src/PriceFilter";
-import VendorFilter from "./src/VendorFilter";
-import SortByFilter from "./src/SortByFilter";
 
 const filtersModuleName = "legato.filters";
 
