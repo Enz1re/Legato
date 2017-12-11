87c5870e1ffc18339ca8715d85b307c8ef13ba9d
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-11 19:11:34 +0200

    Added catch clauses

--- Legato.Client/legato/Components/src/admin/AdminPanelController.ts
+++ Legato.Client/legato/Components/src/admin/AdminPanelController.ts
@@ -4,13 +4,14 @@ import {
     IModalService,
     IManageService,
     IPagingService,
+    IUpdateService
 } from "../../../Interfaces/interfaces";
 
 
 export class AdminPanelController {
-    static $inject = ["ManageService", "ModalService", "PagingService"];
+    static $inject = ["ManageService", "ModalService", "PagingService", "UpdateService"];
 
-    constructor(private manageService: IManageService, private modalService: IModalService, private pagingService: IPagingService) {
+    constructor(private manageService: IManageService, private modalService: IModalService, private pagingService: IPagingService, private updateService: IUpdateService) {
 
     }
 
@@ -18,14 +19,21 @@ export class AdminPanelController {
         this.modalService.openGuitarAddOrEditModal({
             guitar: null,
             type: null
-        }).result.then((guitar: Guitar) => {
-                console.log(guitar);
-        }).catch(() => { })
+        }).result.then(resp => {
+            this.manageService.editGuitarCharacteristics(resp.guitar, resp.type).then(() => {
+                this.updateService.updateData();
+            }).catch(() => {
+                this.modalService.openAlertModal("Failed to add new guitar", "danger");
+            });
+        }).catch(() => { });
     }
 
     changeDisplayAmount() {
         this.modalService.openDisplayAmountModal({ amount: this.pagingService.itemsToShow }).result.then((amount: number) => {
             this.pagingService.itemsToShow = amount;
-        }).catch(() => { })
+            this.updateService.updateData();
+        }).catch(() => {
+            this.modalService.openAlertModal("Failed to change display amount", "danger");
+        })
     }
 }
