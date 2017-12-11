cee0945d43b617ffd52c70553555b367d8162907
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-11 19:10:15 +0200

    Added condition

--- Legato.Client/legato/Components/src/validatePrice/LegatoValidatePriceDirective.ts
+++ Legato.Client/legato/Components/src/validatePrice/LegatoValidatePriceDirective.ts
@@ -3,17 +3,21 @@
     require = "ngModel";
 
     link(scope: ng.IScope, element, attrs, controller: ng.INgModelController) {
-        scope.$watch(attrs.ngModel, (price: any, oldValue: any, scope) => {
+        scope.$watch(attrs.ngModel, (newValue: any, oldValue: any) => {
+            if (!newValue || !oldValue) {
+                return;
+            }
+
             // if value becomes undefined, than it doesn't meet the max-length validation
             // if so, turn it back to its previous value, which has met this requirement
-            if (price.from === undefined) {
-                price.from = oldValue.from;
+            if (newValue.from === undefined) {
+                newValue.from = oldValue.from;
             }
-            if (price.to === undefined) {
-                price.to = oldValue.to;
+            if (newValue.to === undefined) {
+                newValue.to = oldValue.to;
             }
             // From value must be less or equal than To value
-            if (price.from > price.to) {
+            if (newValue.from > newValue.to) {
                 controller.$setValidity("greater", false);
             } else {
                 controller.$setValidity("greater", true);
