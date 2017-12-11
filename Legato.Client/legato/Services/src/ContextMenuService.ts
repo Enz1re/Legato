f1dfece7c9b901cd9bec159019d254962b5eeef3
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-11 19:13:41 +0200

    Added then/catch

--- Legato.Client/legato/Services/src/ContextMenuService.ts
+++ Legato.Client/legato/Services/src/ContextMenuService.ts
@@ -3,19 +3,24 @@
 import {
     IModalService,
     IManageService,
+    IUpdateService,
     IContextMenuService,
 } from "../../Interfaces/interfaces";
 
 
 export default class ContextMenuService implements IContextMenuService {
-    static $inject = ["ManageService", "ModalService"];
+    static $inject = ["ManageService", "ModalService", "UpdateService"];
     menuOptions = [
         {
             text: "Remove",
             click: ($itemScope, $event, modelValue: Guitar, text, $li, data) => {
                 this.modalService.openGuitarDeleteDialog({ guitar: modelValue }).result.then(() => {
-                    this.manageService.removeGuitar(modelValue).then(val => console.log(val));
-                }).catch(() => { });
+                    this.manageService.removeGuitar(modelValue, data.type).then(val => {
+                        this.updateService.updateData();
+                    }).catch(err => {
+                        this.modalService.openAlertModal(`Failed to remove guitar ${modelValue.vendor.name} ${modelValue.model}. ${err.message}`, "danger");
+                    });
+                }).catch(() => {});
             }
         },
         {
@@ -25,13 +30,17 @@ export default class ContextMenuService implements IContextMenuService {
                     guitar: modelValue,
                     type: () => data.type
                 }).result.then(edited => {
-                    this.manageService.editGuitarCharacteristics(edited).then(val => console.log(val));
-                }).catch(() => { });
+                    this.manageService.editGuitarCharacteristics(edited, data.type).then(() => {
+                        this.updateService.updateData();
+                    }).catch(() => { });
+                }).catch(err => {
+                    this.modalService.openAlertModal(`Failed to save changes: ${modelValue.vendor.name} ${modelValue.model}. ${err.message}`, "danger");
+                });
             }
         }
     ];
 
-    constructor(private manageService: IManageService, private modalService: IModalService) {
+    constructor(private manageService: IManageService, private modalService: IModalService, private updateService: IUpdateService) {
 
     }
 }
