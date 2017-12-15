38f15458e75a3e14d1668f8cdc71455013e70bc6
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-15 19:55:42 +0200

    Added service for guitar data

---
+++ Legato.Client/legato/Interfaces/src/IGuitarDataService.ts
@@ -0,0 +1,20 @@
+﻿export interface IGuitarDataService {
+    data: {
+        classical: {
+            stringTypes: string[]
+        },
+        western: {
+            stringNumbers: number[]
+            stringCalibers: number[]
+        },
+        electric: {
+            stringNumbers: number[]
+            soundBoxes: string[]
+            stringCalibers: number[]
+        },
+        bass: {
+            stringNumbers: number[]
+            soundBoxes: string[]
+        }
+    }
+}
---
+++ Legato.Client/legato/Services/src/GuitarDataService.ts
@@ -0,0 +1,49 @@
+﻿import { IGuitarDataService } from "../../Interfaces/interfaces";
+
+
+export default class GuitarDataService implements IGuitarDataService {
+    data: {
+        classical: {
+            stringTypes: string[]
+        },
+        western: {
+            stringNumbers: number[]
+            stringCalibers: number[]
+        },
+        electric: {
+            stringNumbers: number[]
+            soundBoxes: string[]
+            stringCalibers: number[]
+        },
+        bass: {
+            stringNumbers: number[]
+            soundBoxes: string[]
+        }
+    }
+
+    constructor(/* Place here a service to get validation values of guitars' data */) {
+        const classical = {
+            stringTypes: ["Nylon", "Fluorocarbon"]
+        };
+        const western = {
+            stringNumbers: [6, 7, 8, 10, 12],
+            stringCalibers: [8, 9, 10, 11, 12, 13]
+        };
+        const electric = {
+            stringNumbers: [6, 7, 8, 10, 12],
+            soundBoxes: ["Single", "Humbucker"],
+            stringCalibers: [8, 9, 10, 11, 12, 13]
+        };
+        const bass = {
+            stringNumbers: [1, 4, 5, 7, 8, 10],
+            soundBoxes: ["Single", "Humbucker"]
+        };
+
+        this.data = {
+            classical: classical,
+            western: western,
+            electric: electric,
+            bass: bass
+        };
+    }
+}
