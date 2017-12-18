c2d51f74f5064cb3d37b5ef8d3f010de549a43dc
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-18 19:52:11 +0200

    Added new migration

---
+++ Legato.DAL/Migrations/201712181728259_AddedUsers.cs
@@ -0,0 +1,16 @@
+namespace Legato.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedUsers : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
