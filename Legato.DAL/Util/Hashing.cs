98bf3e1a6b0c283bbf9e8f3497d7c45e069cb8c1
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-19 18:57:07 +0200

    Fixed incorrect hashing

--- Legato.DAL/Util/Hashing.cs
+++ Legato.DAL/Util/Hashing.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Security.Cryptography;
 
 
@@ -8,7 +9,18 @@ namespace Legato.DAL.Util
     {
         public static string HashData(string data)
         {
-            return Encoding.UTF8.GetString(SHA1.Create().ComputeHash(Encoding.Unicode.GetBytes(data)));
+            using (var sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(data));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
         }
     }
 }
