85fdd95f68816e000b9fc10dc1015dce404a9f99
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-20 19:23:38 +0200

    Created authentication & authorization filters

--- Legato.Service/Filters/LegatoAuthenticationAttribute.cs
+++ Legato.Service/Filters/LegatoAuthenticationAttribute.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using Ninject;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http.Filters;
 using System.Net.Http.Headers;
 using Legato.Service.Constants;
+using Legato.Service.Interfaces;
 
 
 namespace Legato.Service.Filters
 {
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
-    public class JwtAuthAttribute : Attribute, IAuthenticationFilter
+    public class LegatoAuthenticationAttribute : Attribute, IAuthenticationFilter
     {
+        [Inject]
+        public ILegatoUserServiceWorker ServiceWorker { get; set; }
+
         public bool AllowMultiple => false;
 
         public Task AuthenticateAsync(HttpAuthenticationContext context, CancellationToken cancellationToken)
@@ -22,14 +27,15 @@ namespace Legato.Service.Filters
                 return Task.FromResult(0);
             }
 
-            var claim = JwtManager.GetPrincipal(token);
-            if (claim == null || claim.Identity.Name.ToLower() != "admin")
+            var principal = JwtManager.GetPrincipal(token);
+
+            if (principal == null || !ServiceWorker.FindUser(principal.Identity.Name))
             {
                 context.ErrorResult = new AuthenticationFailureResult(Strings.AccessTokenIsInvalid, context.ActionContext.Request);
                 return Task.FromResult(0);
             }
 
-            return Task.FromResult(claim);
+            return Task.FromResult(principal);
         }
 
         public Task ChallengeAsync(HttpAuthenticationChallengeContext context, CancellationToken cancellationToken)
---
+++ Legato.Service/Filters/LegatoAuthorizeAttribute.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Ninject;
+using System.Net;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+using Legato.Service.Constants;
+using Legato.Service.Interfaces;
+using System.Web.Http.Controllers;
+
+
+namespace Legato.Service.Filters
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class LegatoAuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        private readonly string _currentClaim;
+
+        [Inject]
+        public ILegatoUserServiceWorker ServiceWorker { get; set; }
+
+        public bool AllowMultiple => false;
+
+        public LegatoAuthorizeAttribute(string thisClaim)
+        {
+            _currentClaim = thisClaim;
+        }
+
+        public Task<HttpResponseMessage> ExecuteAuthorizationFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken, Func<Task<HttpResponseMessage>> continuation)
+        {
+            var accessToken = actionContext.Request.Headers.Authorization?.Parameter;
+
+            if (string.IsNullOrEmpty(accessToken))
+            {
+                return Task.FromResult(actionContext.Request.CreateErrorResponse(HttpStatusCode.BadRequest, Strings.AccessTokenIsMissing));
+            }
+            if (!ServiceWorker.IsTokenActive(accessToken))
+            {
+                return Task.FromResult(actionContext.Request.CreateErrorResponse(HttpStatusCode.BadRequest, Strings.AccessTokenIsInvalid));
+            }
+
+            var principal = JwtManager.GetPrincipal(accessToken);
+
+            if (!UserHasClaim(principal.Identity.Name))
+            {
+                return Task.FromResult(actionContext.Request.CreateErrorResponse(HttpStatusCode.Unauthorized, Strings.Unauthorized));
+            }
+
+            return continuation();
+        }
+
+        private bool UserHasClaim(string username)
+        {
+            return ServiceWorker.GetClaims(username).UserClaims.Contains(_currentClaim);
+        }
+    }
+}
