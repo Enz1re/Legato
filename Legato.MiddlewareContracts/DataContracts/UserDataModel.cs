0b4517e60f1bc9d6130c7492447683bd911775df
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-21 18:26:40 +0200

    Added data model and view model for users

---
+++ Legato.MiddlewareContracts/DataContracts/UserDataModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+
+namespace Legato.MiddlewareContracts.DataContracts
+{
+    [DataContract]
+    class UserDataModel
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Username { get; set; }
+
+        [DataMember]
+        public string EncryptedPassword { get; set; }
+
+        [DataMember]
+        public string UserRole { get; set; }
+    }
+}
---
+++ Legato.ServiceDAL/ViewModels/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Legato.ServiceDAL.ViewModels
+{
+    class UserViewModel
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string EncryptedPassword { get; set; }
+
+        public string UserRole { get; set; }
+    }
+}
