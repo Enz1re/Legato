31af0df02bf1944f019cde3719b2a4c4fd500ea6
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-22 17:32:27 +0200

    Guitar delete dialog simplified to yesNoDialog

---
+++ Legato.Client/legato/Components/src/guitarModal/YesNoModalController.ts
@@ -0,0 +1,16 @@
+﻿export class YesNoModalController {
+    text: string;
+    static $inject = ["$uibModalInstance", "text"];
+
+    constructor(private $uibModalInstance: ng.ui.bootstrap.IModalServiceInstance, text: string) {
+        this.text = text;
+    }
+
+    onYesButtonClicked() {
+        this.$uibModalInstance.close();
+    }
+
+    onNoButtonClicked() {
+        this.$uibModalInstance.dismiss();
+    }
+}
