ed6902174aa249748b07569912a2c7b4959886d3
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-22 17:35:14 +0200

    Added user modal and yesNoDialog

--- Legato.Client/legato/Interfaces/src/IModalService.ts
+++ Legato.Client/legato/Interfaces/src/IModalService.ts
@@ -1,8 +1,12 @@
-﻿export interface IModalService {
+﻿import { UserViewModel } from "../../Models/models";
+
+
+export interface IModalService {
     openGuitarModal(resolve?: { [key: string]: string | Object | Function | (string | Function)[] }): ng.ui.bootstrap.IModalServiceInstance;
     openLoginModal(resolve?: { [key: string]: string | Object | Function | (string | Function)[] }): ng.ui.bootstrap.IModalServiceInstance;
     openGuitarAddOrEditModal(resolve?: { [key: string]: string | Object | Function | (string | Function)[] }): ng.ui.bootstrap.IModalServiceInstance;
-    openGuitarDeleteDialog(resolve?: { [key: string]: string | Object | Function | (string | Function)[] }): ng.ui.bootstrap.IModalServiceInstance;
+    openYesNoDialog(text: string): ng.ui.bootstrap.IModalServiceInstance;
     openDisplayAmountModal(resolve?: { [key: string]: string | Object | Function | (string | Function)[] }): ng.ui.bootstrap.IModalServiceInstance;
     openAlertModal(text: string, mode: "success" | "info" | "warning" | "danger"): ng.ui.bootstrap.IModalServiceInstance;
+    openUserModal(users: UserViewModel[] | ng.IPromise<UserViewModel[]>): ng.ui.bootstrap.IModalServiceInstance;
 }
