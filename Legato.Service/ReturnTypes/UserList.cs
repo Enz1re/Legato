3708f5374012e25a7353428ce367c519669d2063
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-22 17:24:44 +0200

    Added a return type for user list

---
+++ Legato.Service/ReturnTypes/UserList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Legato.ServiceDAL.ViewModels;
+
+
+namespace Legato.Service.ReturnTypes
+{
+    public class UserList
+    {
+        public IEnumerable<UserViewModel> Users { get; set; }
+    }
+}
