8765b366849c4781befaec5aac9916cba571a0b7
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-22 17:34:59 +0200

    Added client-side password hashing

--- Legato.Client/legato/Services/src/AuthenticationService.ts
+++ Legato.Client/legato/Services/src/AuthenticationService.ts
@@ -1,23 +1,20 @@
 ﻿import { User } from "../../Models/models";
 
-import {
-    IBase64,
-    IAuthenticationService
-} from "../../Interfaces/interfaces";
+import { IAuthenticationService, ISHA1 } from "../../Interfaces/interfaces";
 
 
 export default class AuthenticationService implements IAuthenticationService {
-    static $inject = ["$http", "$cookies", "$rootScope", "Base64"];
+    static $inject = ["$http", "$cookies", "$rootScope", "SHA1"];
 
-    constructor(private $http: ng.IHttpService, private $cookies: ng.cookies.ICookiesService, private $rootScope, private base64: IBase64) {
+    constructor(private $http: ng.IHttpService, private $cookies: ng.cookies.ICookiesService, private $rootScope, private sha1: ISHA1) {
 
     }
 
     login(username: string, password: string): ng.IPromise<string> {
         return this.$http({
             method: "POST",
-            url: "http://localhost/api/Account/Login",
-            data: { username: username, password: password }
+            url: "http://localhost/api/User/Login",
+            data: { username: username, encryptedPassword: this.sha1.hash(password) }
         }).then((response: ng.IHttpResponse<any>) => {
             return response.data.accessToken;
         });
