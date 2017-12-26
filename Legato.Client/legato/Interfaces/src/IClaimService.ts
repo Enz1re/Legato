e1b799d15f6c2476189c45f5b4f653c01f7605dd
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-26 18:57:51 +0200

    Added user claim validation on client

---
+++ Legato.Client/legato/Interfaces/src/IClaimService.ts
@@ -0,0 +1,10 @@
+﻿export interface IClaimService {
+    canAddGuitar(): ng.IPromise<boolean>;
+    canChangeDisplayAmount(): ng.IPromise<boolean>;
+    canGetUserList(): ng.IPromise<boolean>;
+    canBlockUser(): ng.IPromise<boolean>;
+    canRemoveGuitar(): ng.IPromise<boolean>;
+    canEditGuitar(): ng.IPromise<boolean>;
+    canGetCompromisedAttempts(): ng.IPromise<boolean>;
+    canRemoveCompromiseAttempts(): ng.IPromise<boolean>;
+}
---
+++ Legato.Client/legato/Services/src/ClaimService.ts
@@ -0,0 +1,106 @@
+﻿import { IClaimService, IUserService } from "../../Interfaces/interfaces";
+
+
+export default class ClaimService implements IClaimService {
+    static $inject = ["$http", "UserService"];
+
+    constructor(private $http: ng.IHttpService, private userService: IUserService) {
+
+    }
+
+    canAddGuitar(): ng.IPromise<boolean> {
+        return this.$http.get(`http://localhost/api/Claims/ValidateUserClaim/${this.userService.currentUser.username}/AddGuitar`)
+            .then(() => true)
+            .catch(err => {
+                if (err.statusCode !== 401) {
+                    throw err;
+                } else {
+                    return false;
+                }
+            });
+    }
+
+    canChangeDisplayAmount(): ng.IPromise<boolean> {
+        return this.$http.get(`http://localhost/api/Claims/ValidateUserClaim/${this.userService.currentUser.username}/ChangeDisplayAmount`)
+            .then(() => true)
+            .catch(err => {
+                if (err.statusCode !== 401) {
+                    throw err;
+                } else {
+                    return false;
+                }
+            });
+    }
+
+    canGetUserList(): ng.IPromise<boolean> {
+        return this.$http.get(`http://localhost/api/Claims/ValidateUserClaim/${this.userService.currentUser.username}/GetListOfUsers`)
+            .then(() => true)
+            .catch(err => {
+                if (err.statusCode !== 401) {
+                    throw err;
+                } else {
+                    return false;
+                }
+            });
+    }
+
+    canBlockUser(): ng.IPromise<boolean> {
+        return this.$http.get(`http://localhost/api/Claims/ValidateUserClaim/${this.userService.currentUser.username}/BlockUser`)
+            .then(() => true)
+            .catch(err => {
+                if (err.statusCode !== 401) {
+                    throw err;
+                } else {
+                    return false;
+                }
+            });
+    }
+
+    canRemoveGuitar(): ng.IPromise<boolean> {
+        return this.$http.get(`http://localhost/api/Claims/ValidateUserClaim/${this.userService.currentUser.username}/RemoveGuitar`)
+            .then(() => true)
+            .catch(err => {
+                if (err.statusCode !== 401) {
+                    throw err;
+                } else {
+                    return false;
+                }
+            });
+    }
+
+    canEditGuitar(): ng.IPromise<boolean> {
+        return this.$http.get(`http://localhost/api/Claims/ValidateUserClaim/${this.userService.currentUser.username}/EditGuitar`)
+            .then(() => true)
+            .catch(err => {
+                if (err.statusCode !== 401) {
+                    throw err;
+                } else {
+                    return false;
+                }
+            });
+    }
+
+    canGetCompromisedAttempts(): ng.IPromise<boolean> {
+        return this.$http.get(`http://localhost/api/Claims/ValidateUserClaim/${this.userService.currentUser.username}/GetCompromisedAttempts`)
+            .then(() => true)
+            .catch(err => {
+                if (err.statusCode !== 401) {
+                    throw err;
+                } else {
+                    return false;
+                }
+            });
+    }
+
+    canRemoveCompromiseAttempts(): ng.IPromise<boolean> {
+        return this.$http.get(`http://localhost/api/Claims/ValidateUserClaim/${this.userService.currentUser.username}/RemoveCompromisedAttempts`)
+            .then(() => true)
+            .catch(err => {
+                if (err.statusCode !== 401) {
+                    throw err;
+                } else {
+                    return false;
+                }
+            });
+    }
+}
