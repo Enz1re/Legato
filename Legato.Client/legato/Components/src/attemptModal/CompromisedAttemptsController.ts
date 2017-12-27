88f877f717bc91414987b21e15c65aa8be8e823e
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-27 18:35:55 +0200

    Added attempt modal functionality

--- Legato.Client/legato/Components/src/attemptModal/CompromisedAttemptsController.ts
+++ Legato.Client/legato/Components/src/attemptModal/CompromisedAttemptsController.ts
@@ -2,23 +2,47 @@
 
 import {
     IUserService,
+    IModalService,
     IContextMenuService,
     ICompromisedAttemptHelperService
 } from "../../../Interfaces/interfaces";
 
 
 export class CompromisedAttemptsController {
-    compromisedAttempts: CompromisedAttempt[];
-    static $inject = ["ContextMenuService", "UserService", "CompromisedAttemptHelperService", "compromisedAttempts"];
+    static $inject = ["$uibModalInstance", "ContextMenuService", "UserService", "CompromisedAttemptHelperService", "ModalService", "compromisedAttempts"];
 
-    constructor(private contextMenu: IContextMenuService, private userService: IUserService,
-                private attemptService: ICompromisedAttemptHelperService, compromisedAttempts: CompromisedAttempt[]) {
-
+    constructor(private $uibModalInstance: ng.ui.bootstrap.IModalServiceInstance, private contextMenu: IContextMenuService, private userService: IUserService,
+                private attemptService: ICompromisedAttemptHelperService, private modalService: IModalService, compromisedAttempts: CompromisedAttempt[]) {
+        this.attemptService.allCompromisedAttempts = compromisedAttempts;
     }
 
-    removeAttempt(attempt: CompromisedAttempt) {
-        this.userService.removeCompromisedAttempts([attempt.attemptId]).then(() => {
-
+    removeAttempt(e, attempt: CompromisedAttempt) {
+        e.stopPropagation();
+        this.modalService.openYesNoDialog(`Detele attempt '${attempt.requestDateTime}'? This action is irreversible`).result.then(() => {
+            this.userService.removeCompromisedAttempts([attempt.attemptId]).then(() => {
+                this.attemptService.removeAttempt(attempt);
+            }).catch(err => {
+                this.modalService.openAlertModal(err.data.message, "danger").result.catch(() => { });
+            });
         }).catch(() => { });
+    }
+
+    checkOrUncheckRow(e: Event, model: CompromisedAttempt) {
+        e.stopPropagation();
+        model.isSelected = !model.isSelected;
+        this.checkOrUncheckAttempt(e, model);
+    }
+
+    checkOrUncheckAttempt(e: Event, model: CompromisedAttempt) {
+        e.stopPropagation();
+        if (model.isSelected) {
+            this.attemptService.addAttemptToCheckList(model);
+        } else {
+            this.attemptService.removeAttemptFromCheckList(model);
+        }
+    }
+
+    onOkButtonClicked() {
+        this.$uibModalInstance.close();
     }
 }
