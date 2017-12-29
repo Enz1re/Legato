30847e90bad93fd1010fd834b0b0b1589098b0e1
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-29 18:03:58 +0200

    Added directive for load on scroll

---
+++ Legato.Client/legato/Components/src/loadOnScroll/LegatoLoadOnScrollDirective.ts
@@ -0,0 +1,24 @@
+﻿import angular from "angular";
+
+
+export class LegatoLoadOnScrollDirective implements ng.IDirective {
+    restrict = "A";
+
+    link(scope: ng.IScope, elem: JQLite, attrs: ng.IAttributes) {
+        const scrollOffset = 20;
+        const controller = scope.$eval(attrs.scrollController) || {};
+        const callback = scope.$eval(attrs.scrollCallback).bind(controller) || (() => { });
+
+        elem.on("scroll", (e: JQueryEventObject) => {
+            const shouldScroll = scope.$eval(attrs.scrollIf || "true");
+            if (e.target.scrollTop >= (e.target.scrollHeight - (e.target.clientHeight + scrollOffset)) && shouldScroll) {
+                callback();
+            }
+        });
+    }
+
+    static create(): ng.IDirectiveFactory {
+        const directive = () => new LegatoLoadOnScrollDirective();
+        return directive;
+    }
+}
