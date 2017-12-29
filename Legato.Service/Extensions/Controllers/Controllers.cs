22d78d323abed8ea586958939bd8f80da5ca8fb0
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2017-12-29 12:42:35 +0200

    Added service extensions

---
+++ Legato.Service/Extensions/Controllers/Controllers.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+
+
+namespace Legato.Service.Extensions.Controllers
+{
+    public static class Controllers
+    {
+        //
+        // Summary:
+        //     Creates a Legato.Service.Extensions.Controllers.NotModifiedResult (304 NotModified).
+        //
+        // Returns:
+        //     An Legato.Service.Extensions.Controllers.NotModifiedResult
+        public static IHttpActionResult NotModified(this ApiController controller)
+        {
+            return new NotModifiedResult();
+        }
+    }
+}
---
+++ Legato.Service/Extensions/Controllers/NotModifiedResult.cs
@@ -0,0 +1,16 @@
+﻿namespace Legato.Service.Extensions.Controllers
+{
+    using System.Net;
+    using System.Web.Http;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class NotModifiedResult : IHttpActionResult
+    {
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new HttpResponseMessage(HttpStatusCode.NotModified));
+        }
+    }
+}
