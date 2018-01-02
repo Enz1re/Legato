d7574b56546c9bbafc855cd2b7e9d5c74f6f5700
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2018-01-02 17:58:35 +0200

    Added topPanel

---
+++ Legato.Client/legato/Components/src/topPanel/LegatoTopPanelDirective.ts
@@ -0,0 +1,15 @@
+﻿import { TopPanelController } from "./TopPanelController";
+
+
+export class LegatoTopPanelDirective implements ng.IDirective {
+    restrict = "E";
+    controller = TopPanelController;
+    controllerAs = "topPanelCtrl";
+    templateUrl = "legato/Components/src/topPanel/topPanel.html";
+    scope = {};
+
+    static create(): ng.IDirectiveFactory {
+        const directive = () => new LegatoTopPanelDirective();
+        return directive;
+    }
+}
---
+++ Legato.Client/legato/Components/src/topPanel/TopPanelController.ts
@@ -0,0 +1,24 @@
+﻿import {
+    IUserService,
+    IModalService,
+    IAuthenticationService
+} from "../../../Interfaces/interfaces";
+
+
+export class TopPanelController {
+    static $inject = ["UserService", "AuthenticationService", "ModalService"];
+
+    constructor(private userService: IUserService, private authService: IAuthenticationService, private modalService: IModalService) {
+
+    }
+
+    logIn() {
+        this.modalService.openLoginModal().result.then(() => { }).catch(() => { });
+    }
+
+    logOut() {
+        this.authService.logOff().catch(err => {
+            console.log(err.data.message);
+        });
+    }
+}
