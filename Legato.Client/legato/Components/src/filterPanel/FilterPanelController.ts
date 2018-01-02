2d1d8da180a07f397cc796ce99be6ed83f7280db
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2018-01-02 17:56:41 +0200

    Added filterPanel

---
+++ Legato.Client/legato/Components/src/filterPanel/FilterPanelController.ts
@@ -0,0 +1,30 @@
+﻿import { IUpdateService, IVendorService } from "../../../Interfaces/interfaces";
+
+
+export class FilterPanelController {
+    search: string;
+    static $inject = ["UpdateService", "VendorService"];
+
+    constructor(private updateService: IUpdateService) {
+
+    }
+
+    doSearch(event) {
+        if (event.key.toLowerCase() === "enter") {
+            this.updateService.filter.search = this.search;
+        }
+    }
+
+    unfilter() {
+        this.updateService.filter.search = "";
+        this.updateService.filter.price = { from: null, to: null };
+        this.updateService.filter.sorting = {
+            required: false,
+            name: null,
+            direction: null
+        };
+        this.updateService.filter.vendors.forEach(v => {
+            v.isSelected = true;
+        });
+    }
+}
---
+++ Legato.Client/legato/Components/src/filterPanel/LegatoFilterPanelDirective.ts
@@ -0,0 +1,15 @@
+﻿import { FilterPanelController } from "./FilterPanelController";
+
+
+export class LegatoFilterPanelDirective implements ng.IDirective {
+    restrict = "E";
+    controller = FilterPanelController;
+    controllerAs = "filterCtrl";
+    templateUrl = "legato/Components/src/filterPanel/filterPanel.html";
+    scope = {};
+
+    static create(): ng.IDirectiveFactory {
+        const directive = () => new LegatoFilterPanelDirective();
+        return directive;
+    }
+}
