1cc1ba3a7ef58ce73eb34cd6262f9a8f9970e70b
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2018-01-05 16:57:18 +0200

    Added antiforgery token validation class

---
+++ Legato.Service/Antiforgery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+
+namespace Legato.Service
+{
+    public static class Antiforgery
+    {
+        public static bool ValidateToken(string tokenName, string tokenString)
+        {
+            try
+            {
+                var root = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText($@"{AppDomain.CurrentDomain.BaseDirectory}/settings.json"));
+                return root[tokenName] == tokenString;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
