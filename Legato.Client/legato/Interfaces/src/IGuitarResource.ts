0c49b3eb81eaaab7ad7d48f0f899cc73a91659ca
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2018-01-11 19:42:33 +0200

    Added missing methods to interface

--- Legato.Client/legato/Interfaces/src/IGuitarResource.ts
+++ Legato.Client/legato/Interfaces/src/IGuitarResource.ts
@@ -19,18 +19,22 @@ export interface IGuitarResource {
     // classical guitars
     getClassicalGuitars(filter: Filter, lowerBound: number, upperBound: number): ng.IPromise<ClassicalGuitar[]>;
     getSortedClassicalGuitars(filter: Filter, lowerBound: number, upperBound: number, sortHeader: string, sortDirection: string): ng.IPromise<ClassicalGuitar[]>;
+    searchClassicalGuitars(query: string, lowerBound: number, upperBound: number): ng.IPromise<ClassicalGuitar[]>;
     getClassicalGuitarQuantity(filter: Filter): ng.IPromise<number>;
     // western guitars
     getWesternGuitars(filter: Filter, lowerBound: number, upperBound: number): ng.IPromise<WesternGuitar[]>;
     getSortedWesternGuitars(filter: Filter, lowerBound: number, upperBound: number, sortHeader: string, sortDirection: string): ng.IPromise<WesternGuitar[]>;
+    searchWesternGuitars(query: string, lowerBound: number, upperBound: number): ng.IPromise<WesternGuitar[]>;
     getWesternGuitarQuantity(filter: Filter): ng.IPromise<number>;
     // electric guitars
     getElectricGuitars(filter: Filter, lowerBound: number, upperBound: number): ng.IPromise<ElectricGuitar[]>;
     getSortedElectricGuitars(filter: Filter, lowerBound: number, upperBound: number, sortHeader: string, sortDirection: string): ng.IPromise<ElectricGuitar[]>;
+    searchElectricGuitars(query: string, lowerBound: number, upperBound: number): ng.IPromise<ElectricGuitar[]>;
     getElectricGuitarQuantity(filter: Filter): ng.IPromise<number>;
     // bass guitars
     getBassGuitars(filter: Filter, lowerBound: number, upperBound: number): ng.IPromise<BassGuitar[]>;
     getSortedBassGuitars(filter: Filter, lowerBound: number, upperBound: number, sortHeader: string, sortDirection: string): ng.IPromise<BassGuitar[]>;
+    searchBassGuitars(query: string, lowerBound: number, upperBound: number): ng.IPromise<BassGuitar[]>;
     getBassGuitarQuantity(filter: Filter): ng.IPromise<number>;
     // manage
     add(guitar: Guitar, type: string): ng.IPromise<any>;
