32c867dc5d9a80e7500ce81afd11a276370580c2
kostiantyn.morozov <kostiantyn.morozov@ODS1-LHP-F36940>
2018-01-15 19:59:27 +0200

    Added optional param

--- Legato.Client/legato/Interfaces/src/IUrlParamResolver.ts
+++ Legato.Client/legato/Interfaces/src/IUrlParamResolver.ts
@@ -10,7 +10,7 @@ export interface IUrlParamResolver {
 
     resolvePrice(): Price;
 
-    resolveVendors(vendorList: Vendor[]): Vendor[];
+    resolveVendors(vendorList?: Vendor[]): Vendor[];
 
     resolveSorting(): Sorting;
 
